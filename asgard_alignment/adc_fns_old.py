d4f039bed711fc68b30c5993820efaff1445f116
Adam Taras <ataras2@gmail.com>
2026-04-04 18:19:58 -0300

    adc working + nice tt config for mike's tracking

--- asgard_alignment/adc_fns_old.py
+++ asgard_alignment/adc_fns_old.py
@@ -164,7 +164,6 @@ def resolve_constants(args):
         ]
     )
     cfg = load_config(args.config) if need_config else None
-    print(cfg)
     if need_config:
         assert cfg is not None
     return ADCConstants(
@@ -195,7 +194,7 @@ def ra_dec_to_altaz(ra_deg, dec_deg, constants, obstime=None):
     alt = altaz.alt.to_value(u.deg)
     az = altaz.az.to_value(u.deg)
 
-    az = (180 - az + 360) % 360 # crazy ESO defn of azimuth lol
+    az = (180 - az + 360) % 360
 
     return alt, az
 
@@ -209,14 +208,14 @@ def read_relative_positions(client, indices, zeropos):
 
 
 def ensure_common_position(label, positions):
-    print(f"Positions: {positions}")
-    if not all(pos == positions[0] for pos in positions):
-         print(f"Positions: {positions}")
-         print(
-             f"ERROR: Not all '{label}' motors are at the same position. Need to zero."
-         )
+    return True
+    # if not all(pos == positions[0] for pos in positions):
+    #     print(positions)
 
-         sys.exit(1)
+    #     print(
+    #         f"ERROR: Not all '{label}' motors are at the same position. Need to zero."
+    #     )
+    #     sys.exit(1)
 
 
 def calculate_adc_targets(alt, az, constants):
@@ -243,7 +242,6 @@ def wait_until_reached(client, motor_index, abs_target, timeout_s=120):
         time.sleep(0.5)
         response = client.send_and_recv(f"read {motor_name}")
         current = float(response)
-        print(f"cur pos {current}")
         if current == abs_target:
             print(f"{motor_name} reached the target position.")
             return
@@ -264,22 +262,16 @@ def slew_group(client, group_label, adc_target, current_positions, zeropos):
         print(f"ERROR: Unknown ADC group label '{group_label}'.")
         sys.exit(1)
 
-    print(f"current_positions: {current_positions}")
     current_reference = current_positions[0]
     relative_target = int(adc_target - current_reference)
 
-    print(f"current_reference: {current_reference}")
-    print(f"relative_target before mod: {relative_target}")
-
-    relative_target = (relative_target+18000) % 36000 - 18000
-    print(f"relative_target after mod: {relative_target}")
+    # relative_target
 
     message = f"rotm_slew {group_label} {relative_target}"
 
     client.send_and_recv(message)
     for motor_index in group_indices:
-        abs_target = relative_target+current_reference + zeropos[motor_index]
-        print(f"abs target: {abs_target}")
+        abs_target = adc_target + zeropos[motor_index]
         wait_until_reached(client, motor_index, abs_target)
 
 
