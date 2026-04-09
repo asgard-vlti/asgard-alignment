2b88444ddc3f9d6c89649db802974531fb46e61c
ataras2 <ataras2@gmail.com>
2026-04-09 13:08:41 -0400

    revert lamps from eso

--- asgard_alignment/MultiDeviceServer.py
+++ asgard_alignment/MultiDeviceServer.py
@@ -492,9 +492,10 @@ class MultiDeviceServer:
             ):
                 n_devs_commanded = len(self.instr.devices)  # total number of devices
                 is_all_devs = True
-                dev_names = list(self.instr._motor_config.keys()) + list(
-                    self.instr._lamps_config.keys()
-                )
+                dev_names = list(self.instr._motor_config.keys())
+                # + list(
+                #     self.instr._lamps_config.keys()
+                # )
             else:
                 dev_names = [
                     json_data["command"]["parameters"][i]["device"]
