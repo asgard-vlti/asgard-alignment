d504163edb00cfc2849b79293b09afdc36c925d8
Adam Taras <ataras2@gmail.com>
2026-08-06 19:14:48 -0400

    Fix eng_gui_startup by adding conda bin to PATH (otherwise calling from log viewer on WAG fails)

--- asgard_alignment/cmd_scripts/eng_gui_startup.py
+++ asgard_alignment/cmd_scripts/eng_gui_startup.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 import pathlib
+import shutil
 
 LOCK_FILE = "/tmp/asg.eng_gui.lock"
 def main(redirect=None):
@@ -22,17 +23,24 @@ def main(redirect=None):
         except:
             print(f"Stale lock file found. Ignoring (PID {existing_pid})")
 
-    this_dir = pathlib.Path(__file__).parent
+    os.environ["PATH"] += os.pathsep + os.path.abspath("/home/asg/.conda/envs/asgard/bin/")
+    sys.path.append(os.path.abspath("/home/asg/.conda/envs/asgard/bin/"))
 
-    process = subprocess.Popen([
+    this_dir = pathlib.Path(__file__).parent
+    launch_gui_command = [
+        # "conda", "run", "-n", "asgard", "--no-capture-output",
         "streamlit",
         "run",
         f"{str(this_dir / 'engineering_GUI.py')}",
         "--server.port", str(8501),
         "--server.headless", "true"
-    ], 
-    # stdout=redirect_file, stderr=redirect_file
-    )
+    ]
+    print("Running command:")
+    print(" ".join(launch_gui_command))
+    print(f"streamlit is found at: {shutil.which('streamlit')}")
+    print(f"PATH is {os.getenv('PATH')}")
+
+    process = subprocess.Popen(launch_gui_command)
 
     # Write PID out to lock file
     with open(LOCK_FILE, "w") as fileobj:
@@ -41,4 +49,5 @@ def main(redirect=None):
     return
 
 if __name__ == "__main__":
+
     main(redirect="/home/asg/logs/eng_gui/log.txt")
