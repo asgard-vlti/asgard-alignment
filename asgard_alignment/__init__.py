1ab2aef86737a05e7e54f77046382aa0b076b000
Adam Taras <ataras2@gmail.com>
2026-08-06 17:07:30 -0400

    Fix typo in BALDR phasemark positions defn

--- asgard_alignment/__init__.py
+++ asgard_alignment/__init__.py
@@ -1,1 +1,1 @@
-BALDR_ALLOWED_PHASEMASK_POSITIONS = valid_pos = ["J1", "J2", "J3", "J4", "H2", "H3", "H4", "H5", "LL", "CLEAR"]
+BALDR_ALLOWED_PHASEMASK_POSITIONS = ["J1", "J2", "J3", "J4", "H2", "H3", "H4", "H5", "LL", "CLEAR"]
